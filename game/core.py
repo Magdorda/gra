97c8ab506e8ef84c34417bdbafb4f1b1154d024e
Tomasz Marszałek <tomasz.marszalek@idego.pl>
2018-11-17 20:13:29 +0100

    quickfix`

--- game/core.py
+++ game/core.py
@@ -7,14 +7,11 @@ User interface
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-import person
 
 @app.route('/')
 def hello_world():
-    person = person.Person()
     loc = 'To jest ops lokacji szczegółowy ... w pythonie'
     return render_template('index.html', **{'tomek':'gra', 'location':loc})
 
 if __name__ == "__main__":
     app.run()
-    pass
