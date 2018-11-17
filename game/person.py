b80cb228599a77e28cbbad38c3a9f6cbb09e6dbb
Bobaneiro <h.k.januszewski@gmail.com>
2018-11-17 19:09:13 +0100

    dobre gowno 2

--- game/person.py
+++ game/person.py
@@ -1,6 +1,6 @@
 class Person:
     def __init__(self, strength, brainpower, agility, charisma, perception, vitality, hunger, thirst, fatigue, morale,
-                 health, time):
+                 health, injury, time):
         self.strength = strength
         self.current_strength = strength
         self.brainpower = brainpower
@@ -19,6 +19,8 @@ class Person:
         self.fatigue = fatigue
         self.morale = morale
         self.health = health
+        self.injury = injury
+        self.injury = set()
         self.time = time
 
     def hunger_condition(self):
@@ -29,7 +31,7 @@ class Person:
         penalty2 = self.thirst // 8
         self.current_strength -= penalty1
         self.current_vitality -= penalty1
-        self.morale -= 10 * penalty1
+        self.morale -= 8 * penalty1
         self.current_brainpower -= penalty2
         self.current_agility -= penalty2
         self.current_charisma -= penalty2
@@ -39,10 +41,55 @@ class Person:
         pass
 
     def morale_status(self):
-        pass
+        if self.morale == 0:
+            #self.injury
+            pass
+        if 1 <= self.morale <= 10:
+            pass
 
-    def health_status(self):
+        if 11 <= self.morale <= 30:
+            pass
+
+        if 31 <= self.morale <= 50:
+            self.charisma -= 1
+            self.vitality -= 1
+
+        if 51 <= self.morale <= 70:
+            self.charisma += 1
+            self.vitality += 1
+
+        if 71 <= self.morale <= 90:
+            pass
+
+        if 91 <= self.morale <= 100:
+            pass
+
+    def injuries(self):
         pass
+        # broken_arm
+        # broken_leg
+        # severed_arm
+        # severed_leg
+        # concussion
+        # fever
+        # poisoning
+        # light_wound
+        # deep_wound
+        # internal_wound
+        # broken_jaw
+        # amok
+        # amnesia
+        # skull_fracture
+        # impacted_vision
+        # brain_trauma
+        # burst_eardrum
+        # twitch
+        # mysterious_ailment
+        # missing_eye
+        # megalomania
+        # maimed_hand
+        # confusion
+        # mental_breakdown
 
     def time_flow(self):
         if not self.time % 4:
@@ -54,7 +101,7 @@ class Person:
 
 
 if __name__ == '__main__':
-    mareczek = Person(10, 10, 10, 10, 10, 10, 0, 0, 0, 100, 100, 0)
+    mareczek = Person(10, 10, 10, 10, 10, 10, 0, 0, 0, 100, 100, False, 0)
     mareczek.thirst = 40
     mareczek.thirst_condition()
     print(mareczek.current_strength, mareczek.current_perception, mareczek.morale)
