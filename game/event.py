332ec85a5fad93efbca7482b10ee067096a994c2
Magdorda <you@example.com>
2018-11-17 17:01:08 +0100

    począteczek napsuty

--- game/event.py
+++ game/event.py
@@ -1,2 +1,20 @@
+import random
+
 class Event:
-    pass
+
+    # przyjmuje obiekt klasy materials
+    def found_materials(self, material):
+        m = material.materials_list()
+        print(m)
+        k = random.randint(1, 3)
+        names = random.choices(m, k=k)
+        return names
+
+
+if __name__ == '__main__':
+    from game.materials import Materials
+    m = Materials()
+    e = Event()
+
+    znaleziono = e.found_materials(m)
+    print(znaleziono)
