cda650f703d0bc548503553a250d27a687df2423
Magdorda <you@example.com>
2018-11-17 15:01:56 +0100

    począteczek

---
+++ game/materials.py
@@ -0,0 +1,6 @@
+class Materials():
+    def __init__(self):
+        self.fuel = 1000
+        self.food = 1000
+        self.water = 1000
+        self.screw = 1000
---
+++ game/person.py
@@ -0,0 +1,6 @@
+class Person():
+    def __init__(self):
+        self.strength = 100
+        self.brainpower = 100
+        self.agility = 100
+        self.
