54fd32cf1d41b4e1eed49c2c5428f2052c2682f3
Tomasz Marszałek <tomasz.marszalek@idego.pl>
2018-11-17 20:43:04 +0100

    add test locatons

---
+++ game/locations.py
@@ -0,0 +1,17 @@
+"""
+Contains locations for player
+"""
+
+def level1():
+    return 'Historia Magdy ...'
+
+def level2():
+    return 'Idziesz w kierunku statku koksmicznego'
+
+def levelX():
+    return 'Koniec.'
+
+def game_plot(args):
+    yield level1()
+    yield level2()
+    yield level3()
