c268b767c56255266885b910d0300e0c175a70f6
Bobaneiro <h.k.januszewski@gmail.com>
2018-11-17 21:12:14 +0100

    adenozynotrifosforan

--- game/person.py
+++ game/person.py
@@ -36,14 +36,31 @@ class Person:
         penalty2 = self.thirst // 8
         self.current_strength -= penalty1
         self.current_vitality -= penalty1
-        self.morale -= 8 * penalty1
-        self.current_brainpower -= penalty2
+        self.current_brainpower -= penalty1
+        self.morale -= 6 * penalty1
         self.current_agility -= penalty2
         self.current_charisma -= penalty2
         self.current_perception -= penalty2
 
     def fatigue_condition(self):
-        pass
+        if self.fatigue >= 12:
+            self.current_perception -= 1
+            self.current_agility -= 1
+            self.current_strength -= 1
+            self.current_brainpower -= 1
+            self.current_charisma -= 1
+            self.current_vitality -= 1
+            self.morale -= 2
+            more_fatigue = self.fatigue - 12
+            more_fatigue = more_fatigue // 3
+            self.current_perception -= more_fatigue
+            self.current_agility -= more_fatigue
+            self.current_strength -= more_fatigue
+            self.current_brainpower -= more_fatigue
+            self.current_charisma -= more_fatigue
+            self.current_vitality -= more_fatigue
+            self.morale -= 2 * more_fatigue
+
 
     def morale_status(self):
         if self.morale == 0:
@@ -127,12 +144,12 @@ class Person:
             self.hunger += 1
         if not self.time % 3:
             self.thirst += 1
-        if not self.time % 3:
+        if not self.time % 2:
             self.fatigue += 1
 
 
 if __name__ == '__main__':
-    mareczek = Person(10, 10, 10, 10, 10, 10, 0, 0, 0, 100, 100, False, 0)
+    mareczek = Person(10, 10, 10, 10, 10, 10, 0, 0, 0, 100, 100, 0, False, 0)
     mareczek.thirst = 40
     mareczek.thirst_condition()
     print(mareczek.current_strength, mareczek.current_perception, mareczek.morale)
