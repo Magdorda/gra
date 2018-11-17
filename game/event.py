dd25d4d331b0112947a816d0bb8eefc7c2bac1d6
Magdorda <you@example.com>
2018-11-17 19:05:08 +0100

    począteczek napsuty

--- game/event.py
+++ game/event.py
@@ -1,14 +1,27 @@
 import random
 
-class Event:
 
+class Event:
     # przyjmuje obiekt klasy materials
     def found_materials(self, material):
+        founded = {}
         m = material.materials_list()
-        print(m)
         k = random.randint(1, 3)
         names = random.choices(m, k=k)
-        return names
+        for i in names:
+            amount = random.randrange(1, 150)
+            founded[i] = amount
+        return founded
+
+    def lose_materials(self, material):
+        losed = {}
+        m = material.materials_list()
+        k = random.randint(1, 3)
+        names = random.choices(m, k=k)
+        for i in names:
+            amount = random.randrange(-50, -1)
+            losed[i] = amount
+        return losed
 
 
 if __name__ == '__main__':
@@ -18,3 +31,10 @@ if __name__ == '__main__':
 
     znaleziono = e.found_materials(m)
     print(znaleziono)
+    m.materials_change(znaleziono)
+    print(m.materials_status())
+
+    stracono = e.lose_materials(m)
+    print(stracono)
+    m.materials_change(stracono)
+    print(m.materials_status())
