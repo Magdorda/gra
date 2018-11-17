194e865e3c7636ce68ede2d9663063b7583e2761
Tomasz Marszałek <tomasz.marszalek@idego.pl>
2018-11-17 20:08:56 +0100

    gui

--- game/core.py
+++ game/core.py
@@ -7,9 +7,13 @@ User interface
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+import person
+
 @app.route('/')
 def hello_world():
-    return render_template('index.html', **{'tomek':'gra'})
+    person = person.Person()
+    loc = 'To jest ops lokacji szczegółowy ... w pythonie'
+    return render_template('index.html', **{'tomek':'gra', 'location':loc})
 
 if __name__ == "__main__":
     app.run()
